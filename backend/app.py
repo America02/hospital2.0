c45ed9cbf6774ab5f97b8160dd9101dcb2e46cca
Jesus200995 <jesus.riosgomez@utxicotepec.edu.mx>
2024-07-04 15:30:38 -0600

    Se guardaron codigos en backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routes.persona import persona
 from routes.usuarios import usuario
+from routes.users import user
 
 app = FastAPI()
 app.include_router(persona)
 app.include_router(usuario)
+app.include_router(user)
 print("Bienvenido a mi aplicacion")
