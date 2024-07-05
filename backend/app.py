629e4c2f68ac99f01ea12e71625724effa5e3822
America02 <americayaely.estudillolicona@utxicotepec.edu.mx>
2024-07-05 14:22:29 -0600

    creacion de persons

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from routes.users import user
-
+from routes.persons import person
 
 app = FastAPI()
 
 app.include_router(user)
+app.include_router(person)
 
 print("Bienvenido a mi aplicacion")
