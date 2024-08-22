0f58db6ccdf05cb0ce4e28b5f95ed00c51e42ae7
Jesus2002 <jesus.riosgomez@utxicotepec.edu.mx>
2024-08-22 10:21:27 -0600

    Se modifico correctamente

--- backend/routes/puestos_departamentos.py
+++ backend/routes/puestos_departamentos.py
@@ -16,29 +16,29 @@ def get_db():
     finally:
         db.close()
 
-@puesto_departamento.get("/puestos_departamentos/", response_model=List[schemas.PuestoDepartamento])
+@puesto_departamento.get("/puestos_departamentos/", response_model=List[schemas.PuestoDepartamento], tags=["Puestos Departamentos"], dependencies=[Depends(Portador())])
 def read_puestos_departamentos(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud_puestos_departamentos.get_puestos_departamentos(db, skip, limit)
 
-@puesto_departamento.get("/puesto_departamento/{id}", response_model=schemas.PuestoDepartamento)
+@puesto_departamento.get("/puesto_departamento/{id}", response_model=schemas.PuestoDepartamento, tags=["Puestos Departamentos"], dependencies=[Depends(Portador())])
 def read_puesto_departamento(id: int, db: Session = Depends(get_db)):
     db_puesto_departamento = crud_puestos_departamentos.get_puesto_departamento(db, id)
     if db_puesto_departamento is None:
         raise HTTPException(status_code=404, detail="Puesto departamento no encontrado")
     return db_puesto_departamento
 
-@puesto_departamento.post("/puestos_departamentos/", response_model=schemas.PuestoDepartamento)
+@puesto_departamento.post("/puestos_departamentos/", response_model=schemas.PuestoDepartamento, tags=["Puestos Departamentos"], dependencies=[Depends(Portador())])
 def create_puesto_departamento(puesto_departamento: schemas.PuestoDepartamentoCreate, db: Session = Depends(get_db)):
     return crud_puestos_departamentos.create_puesto_departamento(db, puesto_departamento)
 
-@puesto_departamento.put("/puesto_departamento/{id}", response_model=schemas.PuestoDepartamento)
+@puesto_departamento.put("/puesto_departamento/{id}", response_model=schemas.PuestoDepartamento, tags=["Puestos Departamentos"], dependencies=[Depends(Portador())])
 def update_puesto_departamento(id: int, puesto_departamento: schemas.PuestoDepartamentoUpdate, db: Session = Depends(get_db)):
     db_puesto_departamento = crud_puestos_departamentos.update_puesto_departamento(db, id, puesto_departamento)
     if db_puesto_departamento is None:
         raise HTTPException(status_code=404, detail="Puesto departamento no encontrado, no se pudo actualizar")
     return db_puesto_departamento
 
-@puesto_departamento.delete("/puesto_departamento/{id}", response_model=schemas.PuestoDepartamento)
+@puesto_departamento.delete("/puesto_departamento/{id}", response_model=schemas.PuestoDepartamento, tags=["Puestos Departamentos"], dependencies=[Depends(Portador())])
 def delete_puesto_departamento(id: int, db: Session = Depends(get_db)):
     db_puesto_departamento = crud_puestos_departamentos.delete_puesto_departamento(db, id)
     if db_puesto_departamento is None:
